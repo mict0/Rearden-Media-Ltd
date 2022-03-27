b28413b1fbe49f4fb958e39cd68282f19bd0535a
mict0 <milost1995@gmail.com>
2022-03-27 14:07:45 +0200

    feat(report_generator): campaign impressions filter added

--- ad_report_generator/config.py
+++ ad_report_generator/config.py
@@ -3,3 +3,13 @@ ACTIVE_CAMPAIGN_FILTER = {
     "operator": "IN",
     "value": ["active"],
 }
+ACTIVE_OR_SCHEDULED_CAMPAIGN_FILTER = {
+    "field": "delivery_info",
+    "operator": "IN",
+    "value": ["active", "scheduled"],
+}
+HAS_IMPRESSIONS_FILTER = {
+    "field": "impressions",
+    "operator": "GREATER_THAN",
+    "value": "0",
+}
